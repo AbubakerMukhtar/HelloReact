f89889214c02b35ba0b835b2e6ae7938285d0b31
AbubakerMukhtar <abubakar.mukhtar.3915@gmail.com>
2024-08-31 17:28:42 +0500

    Episode-03 | Laying the foundation Half Lecture

--- App.js
+++ App.js
@@ -2,21 +2,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement('div',{id:"parent"},
-    [React.createElement('div',{id:"child"},
-        [React.createElement('h1',{},"I am heading tag"),
-        React.createElement('h2',{},"I am headingTwo tag")
-        ]),
-    
-    React.createElement('div',{id:"child2"},
-        [React.createElement('h1',{},"I am heading tag2"),
-        React.createElement('h2',{},"I am headingTwo tag2")
-        ]),
-    ]
-);
-console.log("parent",parent);
+// JSX is  like HTML and XML like syntax
+// JSX is  not in React or JS
+// JSX code transpiled before it reaches to JS engine and its done by Bundler --> Bable packge in our bundler
+// JSX ==> Bable transpile it to React.createElement ==> ReactElement-JS Object ==> HTMLElement(render)
+
+const reactElement= React.createElement("h1",{id:"heading"},"This is ReactElement Heading!");
+console.log("reactElement",reactElement);
+const jsxElement= (<h1 id="heading" tabIndex="5">This is jsxElement Heading!</h1>);
+console.log("jsxElement",jsxElement);
+
+//reactElement === jsxElement
+
 root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+root.render(reactElement);
 
 
 
