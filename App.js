1197e1128397f4cd0687e7d41d200bad045f1c02
AbubakerMukhtar <abubakar.mukhtar.3915@gmail.com>
2024-09-01 23:33:15 +0500

    Episode-04 | Talk is Cheap, Show Me the Code

--- App.js
+++ App.js
@@ -2,51 +2,115 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const heading = (<h1 id="heading" tabIndex="5">My React Element is Here!</h1>);
+/**
+ * AppLayout==>
+ * 
+ * Header
+ * -Logo
+ * -Nav Items
+ * Body
+ * - Searchbar
+ * - ResturantCardContainer
+ *  - ResturantCard
+ * Footer
+ * - Copyrights
+ * - Links
+ * - Address
+ * - Contact
+ */
 
-
-//  React Component
-// 1.Class based Component - OLD
-// 2.Functional Component - NEW
-
-
-// Using componets into a component is compostion of componets
-// React Functional Component
-const MyComponent2 = () => <h6>Component 2 is Here!!!!</h6>
-function MyComponent3() {
-    return(
-    <h6>Component 3 is Here!!!!</h6>);
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img
+                    className="logo"
+                    src="https://png.pngtree.com/png-vector/20220705/ourmid/pngtree-food-logo-png-image_5687686.png"
+                    alt="Smoking burger with lettuce logo"
+                />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="logo-container">
+                <img
+                    className="logo"
+                    src="https://png.pngtree.com/png-vector/20220705/ourmid/pngtree-food-logo-png-image_5687686.png"
+                    alt="Smoking burger with lettuce logo"
+                />
+            </div>
+            <div className="nav-items">
+                {/* <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul> */}
+            </div>
+        </div>
+    );
+}
+const ResturantCard = () => {
+    return (
+        <div className="res-card">
+            <div className="card-img-container">
+                <img src="https://images.unsplash.com/photo-1629471722874-13d4208d62ea?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            </div>
+            <div className="dish-container">
+            <h6 className="dish-name">
+                Dish Name
+                </h6><h6 className="rating-stars">
+                    Krados, Johar Town, Lahore, Pakistan 
+                </h6>
+                <h6 className="rating-stars">
+                    3.4 stars
+                </h6>
+                <h6 className="rating-stars">
+                    38 minutes
+                </h6>
+            </div>
+        </div>
+    );
+}
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                <input type="text" placeholder="Search"/>
+                <button>Search</button>
+            </div>
+            <div className="res-container">
+                <ResturantCard />
+                <ResturantCard />
+                <ResturantCard />
+                <ResturantCard />
+                <ResturantCard />
+            </div>
+        </div>
+    );
 }
 
-const MyComponent = () => (
-    <div id="container">
-        <h1>My React Functional Component is Here!!!!</h1>
-        {12 + 18}
-        {heading}
-        <MyComponent2></MyComponent2>
-        <MyComponent3 />
-    </div>
-)
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    );
+}
 
 root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(MyComponent());
-
-
-
-
-
-
-// const headingReact = React.createElement('h1',{id:"heading", attr:'XYZ0'},"This heading is the from React!");\
-/**
- * 
- * <div id="parent">
- *  <div id="child">
- *      <h1>I am heading tag</h1>
- *      <h2>I am headingTwo tag</h2>
- *  </div>
- * </div>
- * 
- * ReactElement(Object)=> HTML(Browser Undersatands)
- * 
- */
+root.render(<AppLayout />);
